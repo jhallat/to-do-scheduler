93809db1d9693878576a21a1284d53129c1450b0
John Hallat <jhallat@gmail.com>
2021-07-20 22:49:31 -0400

    added development enviroment

--- src/main/java/com/jhallat/todo/scheduler/batch/WeeklyScheduleBatch.java
+++ src/main/java/com/jhallat/todo/scheduler/batch/WeeklyScheduleBatch.java
@@ -1,6 +1,7 @@
 package com.jhallat.todo.scheduler.batch;
 
 import com.jhallat.todo.scheduler.*;
+import org.jboss.logging.Logger;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -11,6 +12,8 @@ import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class WeeklyScheduleBatch implements Batch {
+
+    private static final Logger LOG = Logger.getLogger(WeeklyScheduleBatch.class);
 
     @Inject
     ToDoRepository toDoRepository;
@@ -28,35 +31,40 @@ public class WeeklyScheduleBatch implements Batch {
 
     @Override
     public BatchResponse execute(LocalDate date) {
-        AtomicInteger added = new AtomicInteger(0);
-        AtomicInteger updated = new AtomicInteger(0);
-        var currentDay = date.getDayOfWeek();
-        var taskMap = toDoRepository.findAllForToday()
-                .stream()
-                .filter(item -> item.taskId() > 0)
-                .collect(Collectors.toMap(ToDo::taskId, todo -> todo));
-        var weeklySchedules = scheduleRepository.getWeeklySchedules();
-        weeklySchedules.forEach(weeklySchedule -> {
-            if (applicableForDay(weeklySchedule, currentDay)) {
-                var tasks = scheduledTaskRepository.getScheduledTasks(weeklySchedule.id());
-                tasks.forEach(task -> {
-                    if (taskMap.containsKey(task.taskId())) {
-                        var todo = taskMap.get(task.taskId());
-                        toDoRepository.updateQuantity(todo.id(), task.taskQuantity());
-                        updated.getAndIncrement();
-                    } else {
-                        CreateToDoDTO todo = new CreateToDoDTO(task.taskDescription(),
-                                                               task.taskId(),
-                                                               task.taskQuantity(),
-                                                               task.goalId(),
-                                                               task.goalDescription());
-                        toDoRepository.insertToDo(todo);
-                        added.getAndIncrement();
-                    }
-                });
-            }
-        });
-        return new BatchResponse(true, updated.get(), added.get());
+        try {
+            AtomicInteger added = new AtomicInteger(0);
+            AtomicInteger updated = new AtomicInteger(0);
+            var currentDay = date.getDayOfWeek();
+            var taskMap = toDoRepository.findAllForToday()
+                    .stream()
+                    .filter(item -> item.taskId() > 0)
+                    .collect(Collectors.toMap(ToDo::taskId, todo -> todo));
+            var weeklySchedules = scheduleRepository.getWeeklySchedules();
+            weeklySchedules.forEach(weeklySchedule -> {
+                if (applicableForDay(weeklySchedule, currentDay)) {
+                    var tasks = scheduledTaskRepository.getScheduledTasks(weeklySchedule.id());
+                    tasks.forEach(task -> {
+                        if (taskMap.containsKey(task.taskId())) {
+                            var todo = taskMap.get(task.taskId());
+                            toDoRepository.updateQuantity(todo.id(), task.taskQuantity());
+                            updated.getAndIncrement();
+                        } else {
+                            CreateToDoDTO todo = new CreateToDoDTO(task.taskDescription(),
+                                    task.taskId(),
+                                    task.taskQuantity(),
+                                    task.goalId(),
+                                    task.goalDescription());
+                            toDoRepository.insertToDo(todo);
+                            added.getAndIncrement();
+                        }
+                    });
+                }
+            });
+            return new BatchResponse(true, updated.get(), added.get());
+        } catch (Exception exception) {
+            LOG.error("Error occurred in weekly batch", exception);
+            return new BatchResponse(false, 0, 0);
+        }
     }
 
     private boolean applicableForDay(WeeklySchedule weeklySchedule, DayOfWeek currentDay) {
